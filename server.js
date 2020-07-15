fbed7011943f6e5fa720670f9d58296e069b90c2
sabari RS <65396671+Sabari-rs1@users.noreply.github.com>
2020-07-16 01:24:40 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 if(process.env.NODE_ENV!=='production'){
   require('dotenv').config()
 }
-
+var cookieParser = require('cookie-parser')
+const jwt=require('jsonwebtoken')
 const express=require('express')
 const app=express()
 const bcrypt=require('bcrypt')
@@ -12,15 +13,18 @@ const flash=require('express-flash')
 const checkApproved=require('./middleware/checkApproved')
 const {checkRole} =require('./middleware/checkRole')
 const admindb=require('./middleware/adminDb')
+const { response } = require('express')
+const { signedCookie } = require('cookie-parser')
 //const {storeData}=require('./middleware/storeData')
 
-
+let currentUser=null;
 const SESSION_NAME="sid"
 const age=1000*60*60*2
 const inputData=[]
 const dbData=[]
 
-
+// app.use(express.json())
+app.use(cookieParser());
 app.use('/public', express.static('public'));
 app.use(flash())
 app.use(session({
@@ -38,22 +42,40 @@ app.use(express.urlencoded({extended:false}))
 
 
 app.get('/',checkApproved,(req,res)=>{
-  res.render('index.ejs');
+  res.render('index.ejs', {name:currentUser});
 })
 app.get('/login',(req,res)=>{
   res.render('login.ejs');
 })
-app.post('/login',storeData,(req,res)=>{
 
+app.post('/login',storeData,(req,res)=>{
  setTimeout(()=>{
   checkUserData(res,req)
-
  },1000)
 
+ const user={
+  name:req.body.name,
+ password:req.body.password
+}
+
+const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+const refreshToken=jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+res.cookie('AccessToken', accessToken, { maxAge: 8000});
+res.cookie('RefreshToken', refreshToken);
+console.log("Token generated")
 })
+
+
+
+
 app.post('/logout',(req,res)=>{
-  req.session.userID=null;
+  currentUser=null;
   req.session.role=null;
+  const token=req.cookies['AccessToken']
+  const refreshToken=req.cookies['RefreshToken']
+  res.cookie('AccessToken', token, { maxAge: 0});
+  res.cookie('RefreshToken', refreshToken, { maxAge: 0});
+  console.log("Tokens destroyed")
   res.redirect('/login');
   
 })
@@ -82,7 +104,7 @@ catch
 })
 
 
-app.get('/admin',checkRole,storeAdminData,async(req,res)=>{
+app.get('/admin',checkApproved,checkRole,storeAdminData,async(req,res)=>{
 
   setTimeout(()=>{
     res.render('admin.ejs',{posts:adminDb})
@@ -90,20 +112,39 @@ app.get('/admin',checkRole,storeAdminData,async(req,res)=>{
 })
 
 
-app.post('/admin',(req,res)=>{
-  res.send("dfs")
+app.post('/roleChange',(req,res)=>{
   let {inputId,inputRole}= req.body
 let reqUser=null
 User.findOne({ where: { id: inputId } })
   .then(function (person) {
-    // Check if record exists in db
     if (person) {
       person.update({
         role: inputRole
       })
+      
      }
   })
+  setTimeout(reloadAdminDB,'1000')
+  req.flash('error','Successful Role Change')
+  res.redirect('/admin')
+})
 
+
+app.post('/deleteEmployee',(req,res)=>{
+  let {inputId,inputRole}= req.body
+let reqUser=null
+User.findOne({ where: { id: inputId } })
+  .then(function (person) {
+    if (person) {
+      person.destroy()
+      setTimeout(reloadAdminDB,'1000')
+      req.flash('error','Employee Removed')
+      res.redirect('/admin')
+    }else{
+      req.flash('error','No such Employee')
+      res.redirect('/admin')
+    }
+  })
 })
 
 
@@ -161,33 +202,47 @@ function storeData(req,res,next)
   .then((persons)=>{
     try{
       dbData.push(persons[0].dataValues)
+      currentUser=dbData[0].name;
+      console.log("passed")
     }catch(err)
     {
       console.log("no such user found")
     }
   })
-  
+     //jwt goes here
+
   return next()
 }
 
 const adminDb=[]
 function storeAdminData(req,res,next) 
 {
+  adminDb.length=0;
   User.findAll({
     order: [ [ 'createdAt', 'DESC' ]]
   }).then(function(entries){
    for(let i=0;i<entries.length;i++)
    {
      adminDb.push(entries[i].dataValues)
-     
    }
    console.log(adminDb)
-  }); 
+  });
   return next()
 }
 
 
-
+function reloadAdminDB()
+{
+  adminDb.length=0;
+  User.findAll({
+    order: [ [ 'createdAt', 'DESC' ]]
+  }).then(function(entries){
+   for(let i=0;i<entries.length;i++)
+   {
+     adminDb.push(entries[i].dataValues)
+   }
+  });
+}
 
 
 
